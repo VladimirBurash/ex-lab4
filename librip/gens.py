a878bd3d494b57bf4de7f774227adc65d4d9300f
VladimirBurash <VladimiBurash@mail.ru>
2016-11-24 14:28:46 +0300

    Update gens.py

--- librip/gens.py
+++ librip/gens.py
@@ -2,23 +2,27 @@ import random
 
 
 # Генератор вычленения полей из массива словарей
-# Пример:
-# goods = [
-#    {'title': 'Ковер', 'price': 2000, 'color': 'green'},
-#    {'title': 'Диван для отдыха', 'price': 5300, 'color': 'black'}
-# ]
-# field(goods, 'title') должен выдавать 'Ковер', 'Диван для отдыха'
-# field(goods, 'title', 'price') должен выдавать {'title': 'Ковер', 'price': 2000}, {'title': 'Диван для отдыха', 'price': 5300}
-
 def field(items, *args):
-    assert len(args) > 0
-    # Необходимо реализовать генератор 
+	assert len(args) > 0
+	
+	for n in items:
+		if len(args) == 1:
+			if not n.get(args[0]) is None:
+				yield n.get(args[0]);
+		else:
+			new_items = {};
+			for x in args:
+				if not n.get(x) is None:
+					new_items[x] = n[x]
+
+			if len(new_items) > 0:
+				yield new_items;
+
 
 
 # Генератор списка случайных чисел
-# Пример:
-# gen_random(1, 3, 5) должен выдать примерно 2, 2, 3, 2, 1
-# Hint: реализация занимает 2 строки
 def gen_random(begin, end, num_count):
-    pass
-    # Необходимо реализовать генератор
+	a = 0;
+	while a < num_count:
+		yield begin + round(random.random()*(end - begin))
+		a += 1
