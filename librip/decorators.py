4c7026ae840a10b2f8c76cd8bfee305e5c22e6bc
VladimirBurash <VladimiBurash@mail.ru>
2016-11-24 14:28:25 +0300

    Update decorators.py

--- librip/decorators.py
+++ librip/decorators.py
@@ -1,37 +1,19 @@
-# Здесь необходимо реализовать декоратор, print_result который принимает на вход функцию,
-# вызывает её, печатает в консоль имя функции, печатает результат и возвращает значение
-# Если функция вернула список (list), то значения должны выводиться в столбик
-# Если функция вернула словарь (dict), то ключи и значения должны выводить в столбик через знак равно
-# Пример из ex_4.py:
-# @print_result
-# def test_1():
-#     return 1
-#
-# @print_result
-# def test_2():
-#     return 'iu'
-#
-# @print_result
-# def test_3():
-#     return {'a': 1, 'b': 2}
-#
-# @print_result
-# def test_4():
-#     return [1, 2]
-#
-# test_1()
-# test_2()
-# test_3()
-# test_4()
-#
-# На консоль выведется:
-# test_1
-# 1
-# test_2
-# iu
-# test_3
-# a = 1
-# b = 2
-# test_4
-# 1
-# 2
+def print_result(func):
+	def wrapper(*args, **kwargs):
+		
+		result = func(*args, **kwargs)
+
+		print(func.__name__)
+		if type(result) == dict:
+			for i in result.keys():
+				print(i, " = ", result[i])
+		elif type(result) == list:
+			for i in result:
+				print(i)
+		elif type(result) == tuple:
+			if len(result) == 1:
+				print(result[0])
+		else:
+			print(result)
+		return result
+	return wrapper
