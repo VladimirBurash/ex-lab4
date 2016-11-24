71c094244ac00157c30c93c4242413a5184f3b24
VladimirBurash <VladimiBurash@mail.ru>
2016-11-24 14:31:39 +0300

    Update ex_4.py

--- ex_4.py
+++ ex_4.py
@@ -1,11 +1,9 @@
 from librip.decorators import print_result
 
-# Необходимо верно реализовать print_result
-# и задание будет выполнено
 
 @print_result
-def test_1():
-    return 1
+def test_1(a):
+    return a
 
 
 @print_result
@@ -23,7 +21,7 @@ def test_4():
     return [1, 2]
 
 
-test_1()
+test_1(1)
 test_2()
 test_3()
 test_4()
