b2701a70b3b2bbcacfd19d8ac866498f903796c5
VladimirBurash <VladimiBurash@mail.ru>
2016-11-24 14:31:05 +0300

    Update ex_2.py

--- ex_2.py
+++ ex_2.py
@@ -1,8 +1,30 @@
 #!/usr/bin/env python3
-from librip.gens import gen_random
+from librip.gen import gen_random
 from librip.iterators import Unique
 
 data1 = [1, 1, 1, 1, 1, 2, 2, 2, 2, 2]
 data2 = gen_random(1, 3, 10)
+data3 = ["a", "A", 'B', 'b']
+data4 = ["Nna", "nna", None]
 
 # Реализация задания 2
+
+for i in Unique(data1):
+	print(i)
+print()
+
+for i in Unique(data2):
+	print(i)
+print()
+
+for i in Unique(data3, ignore_case = True):
+	print(i)
+print()
+
+for i in Unique(data4, ignore_case1 = True):
+	print(i)
+print()
+
+
+for i in Unique(data4):
+	print(i)
